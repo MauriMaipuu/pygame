e799e22e629e60e72b81d4f78f75cbdcc67ad4db
MauriMaipuu <115622564+MauriMaipuu@users.noreply.github.com>
2023-03-06 11:20:15 +0200

    Update YL1.py

--- E-6ppe/YL1.py
+++ E-6ppe/YL1.py
@@ -3,7 +3,7 @@
 #tegelikult prinditakse a/n 2 korda
 
 a = 1  #a-le antakse väärtus 1
-b = 'a/n'  #b-le antakse sõneväärtus a/n
+b = 'a\n'  #b-le antakse sõneväärtus a/n
 while a < 3:  # kui a on väiksem kui 3
     print(b)  # prinditakse b
     a += 1 #a inkrem
